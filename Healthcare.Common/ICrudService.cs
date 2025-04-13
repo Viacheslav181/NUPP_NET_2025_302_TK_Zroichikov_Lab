d6adacbe2709af517f2d59fc18fb1a9822228515
Viacheslav181 <161226786+Viacheslav181@users.noreply.github.com>
2025-04-13 21:48:26 +0300

    Add files via upload

---
+++ Healthcare.Common/ICrudService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare.Common
+{
+    public interface ICrudService<T>
+    {
+        void Create(T item);
+        T Read(Func<T, bool> predicate);
+        IEnumerable<T> ReadAll();
+        void Update(Func<T, bool> predicate, Action<T> updateAction);
+        void Delete(Func<T, bool> predicate);
+        void Save(string filePath);
+        void Load(string filePath);
+    }
+}
---
+++ Healthcare.Common/Patient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare.Common
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string PatientId { get; set; }
+        public string Diagnosis { get; set; }
+        public string InsuranceId { get; set; }
+
+        public Patient() { }
+
+        public Patient(string name, string gender, int age, string patientId, string diagnosis, string insuranceId)
+        {
+            Name = name;
+            Gender = gender;
+            Age = age;
+            PatientId = patientId;
+            Diagnosis = diagnosis;
+            InsuranceId = insuranceId;
+        }
+
+        public string GetInfo()
+        {
+            return $"{Name}, {Gender}, Age: {Age}, ID: {PatientId}, Diagnosis: {Diagnosis}, Insurance: {InsuranceId}";
+        }
+    }
+}
+
